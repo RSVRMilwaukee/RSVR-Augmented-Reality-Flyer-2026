7878131b587d039c742bb2992e400f8b5ab52d96
RSVRMilwaukee <Rspiering@RSVRMilwaukee.com>
2026-07-28 14:57:34 -0700

    Initial WebGL AR build (iTracker 6 template, RSVR flyer target)

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-AR for Web Project-0.1.0";
+const contentToCache = [
+    "Build/AR for Web 2026.loader.js",
+    "Build/AR for Web 2026.framework.js.unityweb",
+    "Build/AR for Web 2026.data.unityweb",
+    "Build/AR for Web 2026.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
